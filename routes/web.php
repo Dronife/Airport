405a4ee357fdc40e795ff121ce587e385ae2d9ae
Dronife <d.smeledis@gmail.com>
2021-11-01 13:54:59 +0200

    Laravel bootstrap auth ui

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/test', function () {
     dd(AirStation::find(1)->allRountes());
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
