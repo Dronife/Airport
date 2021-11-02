2b6a7645cb602a7b63a2833bbde32429c1c65015
Dronife <d.smeledis@gmail.com>
2021-11-02 19:37:31 +0200

    Only with auth can navigate

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Airline;
 use App\Models\AirStation;
 use Illuminate\Support\Facades\Route;
 
@@ -13,16 +12,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
+ */
 
 Route::get('/test', function () {
     dd(AirStation::find(1)->allRountes());
 });
 
 Auth::routes();
-Route::resource('countries',App\Http\Controllers\CountryController::class);
-Route::resource('airports',App\Http\Controllers\AirportController::class);
-Route::resource('airlines',App\Http\Controllers\AirlineController::class);
-Route::resource('airline-routes',App\Http\Controllers\AirlineRouteController::class);
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('countries', App\Http\Controllers\CountryController::class);
+    Route::resource('airports', App\Http\Controllers\AirportController::class);
+    Route::resource('airlines', App\Http\Controllers\AirlineController::class);
+    Route::resource('airline-routes', App\Http\Controllers\AirlineRouteController::class);
+});
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
